d7791974a501230c36304d633e218d818617622e
wonho <wonho1401@naver.com>
2022-01-03 13:55:06 +0900

    MongoDB, Mongoose, User schema 생성

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://w0no:gh1163229@boilerplate.6pfbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
