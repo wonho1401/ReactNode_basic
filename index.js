84f3b871bf67734b6a73902a10f4e4e66ad7770e
wonho <wonho1401@naver.com>
2022-01-04 13:30:54 +0900

    body-parser, postman

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const { User } = require("./models/User");
 
 const mongoose = require("mongoose");
 mongoose
@@ -16,5 +22,19 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입할 때 넣는 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
