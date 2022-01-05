ff7ae51343deadf7a77a2ba3961982269a98de95
wonho <wonho1401@naver.com>
2022-01-05 15:06:04 +0900

    Tokenization using bcrypt

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.post("/register", (req, res) => {
 
   const user = new User(req.body);
 
+  
+
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
