50f0212679d765c49d79c2579c55de766904b863
wonho <wonho1401@naver.com>
2022-01-17 13:59:40 +0900

    axios

--- client/src/components/views/LandingPage/landingPage.js
+++ client/src/components/views/LandingPage/landingPage.js
@@ -1,6 +1,10 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import axios from "axios";
 function LandingPage() {
+  //useEffect는 LandingPage에 들어오자마자 그 안의 것을 실행한다는 뜻.
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => console.log(response));
+  }, []);
   return <div>Landing Page</div>;
 }
 
