3e3cdd28958f272bf8dd02a1ddfbc4c765bbb662
wonho <wonho1401@naver.com>
2022-02-03 20:10:40 +0900

    login page with redux

--- client/src/components/views/LoginPage/loginPage.js
+++ client/src/components/views/LoginPage/loginPage.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../../_actions/user_action";
 
-function LoginPage() {
-  return <div>Login</div>;
+function LoginPage(props) {
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault(); //refresh 되는 것을 방지하는 것.
+    let body = {
+      email: Email,
+      password: Password,
+    };
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        navigate("/");
+      } else {
+        alert("Error");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email </label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password </label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
