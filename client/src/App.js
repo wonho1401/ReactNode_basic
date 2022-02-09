b72fc96c523429e21bfb1fcef9757d8617d0400c
wonho <wonho1401@naver.com>
2022-02-09 15:47:51 +0900

    hoc

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,15 @@ import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 import Auth from "./hoc/auth";
 
 function App() {
+  const NewLandingPage = Auth(LandingPage, null);
+  const NewLoginPage = Auth(LoginPage, false);
+  const NewRegisterPage = Auth(RegisterPage, false);
   return (
     <Router>
       <Routes>
-        <Route path="/*" element={<LandingPage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/*" element={<NewLandingPage />} />
+        <Route path="/login" element={<NewLoginPage />} />
+        <Route path="/register" element={<NewRegisterPage />} />
       </Routes>
     </Router>
   );
--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,25 +1,44 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../_actions/user_action";
 
-export default function (specificComponent, option, adminRoute = null) {
+export default function (SpecificComponent, option, adminRoute = null) {
+  //option 의 경우
+  // 1. null => 아무나 출입이 가능한 페이지
+  // 2. true => 로그인한 유저만 출입이 가능한 페이지
+  // 3. false => 로그인한 유저는 출입이 불가능한 페이지
 
-    //option 의 경우
-    // 1. null => 아무나 출입이 가능한 페이지
-    // 2. true => 로그인한 유저만 출입이 가능한 페이지
-    // 3. false => 로그인한 유저는 출입이 불가능한 페이지
-    
   function AuthenticationCheck(props) {
     const dispatch = useDispatch();
+    let navigate = useNavigate();
 
     useEffect(() => {
       dispatch(auth()).then((response) => {
         console.log(response);
+        //분기처리 해야함
+
+        // 로그인하지 않은 상태
+        if (!response.payload.isAuth) {
+          if (option) {
+            navigate("/login");
+          }
+        } else {
+          // 로그인한 상태
+          if (adminRoute && !response.payload.isAdmin) {
+            navigate("/");
+          } else {
+            if (option === false) {
+              navigate("/");
+            }
+          }
+        }
       });
 
-      axios.get("/api/users/auth");
+      //   axios.get("/api/users/auth");
     }, []);
+    return <SpecificComponent />;
   }
   return AuthenticationCheck;
 }
